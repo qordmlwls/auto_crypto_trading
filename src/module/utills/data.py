32fbcb795640aac30d30636bbe243c46fc17067d
Ubuntu <ubuntu@ip-172-26-5-112.ap-northeast-2.compute.internal>
2023-02-25 02:58:35 +0000

    feat: multiprocessing edit

--- src/module/utills/data.py
+++ src/module/utills/data.py
@@ -46,7 +46,7 @@ def parallelize_list_to_df(data_list: List, func):
     if len(data_list) < num_cores:
         num_cores = len(data_list)
         # for avoiding os.fork() cannot allocate memory error
-        num_cores = int(num_cores / 2)
+        num_cores = int(num_cores / 3)
     data_split = np.array_split(data_list, num_cores)
     pool = Pool(num_cores)
     df = pd.concat(pool.map(func, data_split))
